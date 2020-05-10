885716ea7cecf0f4c0109c044d2e4f7b78d2e5b4
ViniciusNyari <viniciusnyari@gmail.com>
2020-05-10 17:45:01 -0300

    FE - Introdução ao React - Importando CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //Toda vez que tiver presença de html, precisa ter o react importado
 import React from 'react';
+import './App.css';
 
 function App(){
   return <h1> Hello World em componente!</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,13 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+      ]
       }
     ]
   }
