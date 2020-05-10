02e611d67b301cfe7dfcde6bc05b2c46c44a6184
ViniciusNyari <viniciusnyari@gmail.com>
2020-05-10 20:07:50 -0300

    FE - Introdução ao React - Estado & Imutabilidade

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,7 +1,11 @@
 import React, { Component} from 'react';
 
 class TechList extends Component {
+  
+  //Todo array de estado 'state' é imutável e não pode ser alterado diretamente
+  //por isso precisamos dos métodos 'setState'
   state = {
+    newTech: '',
     techs: [
       'Node.js',
       'ReactJS',
@@ -9,14 +13,38 @@ class TechList extends Component {
     ]
   };
 
+  //Precisa ser no formato de 'arrow function' fois senão não tem acesso a 
+  //variável 'this'
+  handleInputChange = e => {
+    this.setState( { newTech: e.target.value});
+  }
+
+  handleSubmit = e => {
+    //N]ao dá refresh na tela
+    e.preventDefault();
+    //spreadoperatior '...'
+    //tem que recriar o array
+    this.setState({ 
+      techs: [... this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  }
+
+  // <> é chamado de fragment no React - ao invésde usar uma div
+  // coloca o conteudo direto na tag principal
   render() {
-    console.log(this.state);
     return (
-      <ul>
-        <li> Node.js</li>
-        <li> ReactJS</li>
-        <li> React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          { this.state.techs.map(tech=> <li key={tech}>{tech}</li>)}
+        </ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value={ this.state.newTech}
+          />
+          <button type="submit">Enviar</button>
+      </form>      
     );
   }
 }
